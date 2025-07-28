ce2ab047781749bbddd8b6cdecbf564737aba391
Daniel Zhu <danielfangzhu@gmail.com>
2025-07-28 14:18:50 -0700

    initial homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        {children}
+        <div className="absolute z-0 inset-0 h-full w-full bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
+        <div className="z-10">{children}</div>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <div className="h-screen outline flex flex-col align-center justify-center text-center px-40">
+      <div className="h-screen outline flex flex-col items-center justify-center text-center">
         <h1 className="text-6xl font-bold">CodeSprings</h1>
-        <p className="text-xl font-medium mt-6">
+        <p className="text-xl font-medium mt-6 max-w-2xl">
           By providing both high-quality programming lessons and requisite
           resources for free, we empower underprivileged youth across the world
           to foster innovation and create pathways to brighter futures.
